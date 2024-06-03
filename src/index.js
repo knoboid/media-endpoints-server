12dbfacc9ef508c994f7ffc933b72342527afb67
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-06-03 17:45:58 +0100

    Make the server take a simple object.
    Added getDefaultsFromServerConfig

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import https from "https";
 import express from "express";
 import { wsServer } from "./ws/ws-server.js";
+import { getCredentials } from "./security/https-credentials.js";
 
-export function startServer(serverRoot, serverOptions) {
-  if (typeof serverRoot === "undefined") {
-    serverRoot = "./src";
+function getDefaultsFromServerConfig(serverConfig) {
+  if (typeof serverConfig === "undefined") {
+    serverConfig = {};
   }
+  if (typeof serverConfig.serverRoot === "undefined") {
+    serverConfig.serverRoot = "./public";
+  }
+  if (typeof serverConfig.serverOptions === "undefined") {
+    serverConfig.serverOptions = getCredentials();
+  }
+  return serverConfig;
+}
+
+export function startServer(serverConfig) {
+  const { serverRoot, serverOptions } =
+    getDefaultsFromServerConfig(serverConfig);
+
   const app = express();
 
   app.use(express.static(serverRoot));
