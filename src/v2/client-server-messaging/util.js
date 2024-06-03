10118c6a35d3f12c0e9ff87b4296dd2d6ba91142
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-06-03 16:38:03 +0100

    Removed unused signalling code

--- src/v2/client-server-messaging/util.js
+++
@@ -1,5 +0,0 @@
-export function broadcastToClientGroup(clientGroup, type, payload) {
-  clientGroup.getWebSockets().forEach((webSocket) => {
-    webSocket.send(JSON.stringify({ type, payload }));
-  });
-}
